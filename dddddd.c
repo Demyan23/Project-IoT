61f438cefd65f5fd8c4ba6f538a1a10610a06d18
Demyan23 <92452073+Demyan23@users.noreply.github.com>
2021-10-27 22:07:12 +0300

    Update dddddd.c

--- dddddd.c
+++ dddddd.c
@@ -1,29 +1,22 @@
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
 
-void main(void)
+int main()
 {
-double a,b,h,x,y,d,s;
-int n;
-printf("input a= "); 
-scanf("%lf",&a);
-printf("input b= "); 
-scanf("%lf",&b);
-printf("input h= ");
-scanf("%lf",&h);
-printf("input d= ");
-scanf("%lf",&d);
-for(x=a;x<b;x=x+h)
-{ 
-    n=0;
-    s=0;
-    do
-    {
-        y=pow((x-1),(2*n+1))/((2*n+1)*pow((x+1),(2*n+1)));
-        s=s+y;
-        n=n+1;
-    }
-    while(fabs(y)>d);
-    printf("\n x=%lf s=%lf",x,2*s);
-}
+  char x[10] = "dfsdddfsaf";
+  for (int i = 0; i < 10; i++)
+  {
+    printf("%c", x[i]);
+  }
+  printf("\n");
+  int WhatShouldIChange[3] = {1,2,5};
+  for (int i = 0; i < 3; i++)
+  {
+    x[WhatShouldIChange[i]] -= 32;
+  }
+  for (int i = 0; i < 10; i++)
+  {
+    printf("%c", x[i]);
+  }
+  return 0;
 }
